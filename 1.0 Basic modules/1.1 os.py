51d27f600623134a92f21deb07d9c6469a74ab25
SUYASH JAGTAP <119748227+suyash3636@users.noreply.github.com>
2025-07-29 05:18:00 +0000

    Learning

---
+++ 1.0 Basic modules/1.1 os.py
@@ -0,0 +1,33 @@
+import os
+# get current directory
+# print(os.getcwd())
+
+# get directory cintents
+# print(os.listdir())
+
+
+# create folder
+# os.mkdir( "1.2 json.py")
+
+
+# delete folder
+# os.rmdir("1.2 json.py")
+
+
+# create file
+# with open("1.2 json.py","w") as f:
+#     f.write("# contents for json module")
+
+
+# delete file
+# os.remove("1.2 json.py")
+
+# Rename or move files
+
+# os.rename("old.txt", "new.txt")
+# If you give it a path:
+
+ 
+# os.rename("file.txt", "backup/file.txt") 
+
+
---
+++ 1.0 Basic modules/1.2 json.py
@@ -0,0 +1,16 @@
+import json
+# contents for json module
+data = {"Name": "Ash", "age":18}
+json_str= json.dumps(data)
+
+# Json data is in string datatype in dictionary format
+# print(type(json_str))
+# print(json_str)
+
+
+# converting json into dictionary object
+# data_from_json = json.loads(json_str)
+# print(type(data_from_json))
+# print(data_from_json)
+
+
